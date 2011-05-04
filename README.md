b8a12adc11b469488e98798dfe827884db002b5a
Volodymyr Hotsyk <gotsyk@gmail.com>
2011-05-05 00:10:34 +0300

    Simple and dirty python fork of get-shit-done php script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# get-shit-done
+# get-shit-done.py - is the python clone of the get-shit-done php script
 get-shit-done is an easy to use command line program that blocks websites known to distract us from our work.
 
 After cloning this repository, put it in your $PATH and ensure it is executable.
@@ -11,12 +11,12 @@ Execute it as root because it modifies your hosts file and restarts your network
 ## To no longer get-shit-done
 `sudo get-shit-done play`
 
-### $siteList
+### SITE_LIST
 Add or remove elements of this array for sites to block or unblock.
 
-### $restartNetworkingCommand
+### RESTART_NETWORK
 Update this variable with the path to your network daemon along with any parameters needed to restart it.
 
-### $hostsFile
+### HOST_FILE
 Update this variable to point to the location of your hosts file. Make sure it is an absolute path.
 
---
+++ get-shit-done.py
@@ -0,0 +1,103 @@
+#!/bin/python
+
+import argparse
+import getpass
+
+SITE_LIST=[
+    'reddit.com',
+    'forums.somethingawful.com',
+    'somethingawful.com',
+    'digg.com',
+    'break.com',
+    'news.ycombinator.com',
+    'infoq.com',
+    'bebo.com',
+    'twitter.com',
+    'facebook.com',
+    'fb.com'
+    'blip.com',
+    'youtube.com',
+    'vimeo.com',
+    'delicious.com',
+    'flickr.com',
+    'friendster.com',
+    'hi5.com',
+    'linkedin.com',
+    'livejournal.com',
+    'meetup.com',
+    'myspace.com',
+    'plurk.com',
+    'stickam.com',
+    'stumbleupon.com',
+    'yelp.com',
+    'slashdot.com',
+    'vk.com', 'vkontakte.ru',
+    'korrespondent.net',
+    'lenta.ru',
+    'habrahabr.ru',
+    'bash.org.ru',
+    'bash.org.ua',
+]
+
+HOST_FILE = '/etc/hosts';
+START_TOKEN = '## start-gsd';
+END_TOKEN = '## end-gsd';
+
+
+class ModeAction(argparse.Action):
+    def __call__(self, parser, namespace, values=None, option_string=None):
+        if getpass.getuser() != 'root':
+            raise Exception('Script should be executed as the root')
+        if values == 'work':
+            f = open(HOST_FILE, 'r')
+            content = f.read()
+            f.close()
+            if START_TOKEN in content and END_TOKEN in content:
+                raise Exception('Work mode already set')
+            elif START_TOKEN in content or END_TOKEN in content:
+                raise Exception('Smth goes wrong - 1 token is in file, but second - not')
+            else:
+                f = open(HOST_FILE, 'w')
+                new_content = []
+                new_content.append(START_TOKEN+'\n')
+                for site in SITE_LIST:
+                    new_content.append("127.0.0.1\t%s\n" % site)
+                    new_content.append("127.0.0.1\twww.%s\n" % site)
+                new_content.append(END_TOKEN+'\n')
+                f.write(content+"".join(new_content))
+
+        elif values == 'play':
+            f = open(HOST_FILE, 'r')
+            content = f.read()
+            f.close()
+            if not START_TOKEN in content and not END_TOKEN in content:
+                raise Exception('Play mode already set')
+            elif START_TOKEN in content and END_TOKEN in content:
+                f = open(HOST_FILE, 'r')
+                lines = f.readlines()
+                f.close()
+                f = open(HOST_FILE, 'w')
+                inside_token = False
+                new_content = []
+                for line in lines:
+                    if START_TOKEN in line:
+                        inside_token = True
+                    if not inside_token:
+                        new_content.append(line)
+                    if END_TOKEN in line:
+                        inside_token = False
+                f.write("".join(new_content))
+            elif START_TOKEN in content or END_TOKEN in content:
+                raise Exception('Smth goes wrong - 1 token is in file, but second - not')
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Get shit done.')
+    parser.add_argument('-m, --mode', action=ModeAction,
+                        choices=['work', 'play'],
+                        help='switch to mode')
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
